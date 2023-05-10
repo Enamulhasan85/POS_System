d5b408d65beb76d2641046799286758df2db693b
rubelahmod <rubelahmod320@gmail.com>
2023-05-10 12:51:00 +0600

    Updated_AfterProjectShow

--- Grocery Point of Sale System/core/admin.py
+++ Grocery Point of Sale System/core/admin.py
@@ -1,41 +1,41 @@
 from django.contrib import admin
-# from .models import UserProfile, Category, Product, Cart, Order, OrderProduct, Address, Payment, Review
+from .models import UserProfile, Category, Product, Cart, Order, OrderProduct, Address, Payment, Review
 
 # Register your models here.
 
-# class ProfileAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'stripe_customer_id', )
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'stripe_customer_id', )
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('name', )
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'price', 'category', )
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category', )
 
-# class CartAdmin(admin.ModelAdmin):
-#     list_display = ('item', 'quantity', 'user', )
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('item', 'quantity', 'user', )
 
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'start_date', 'shipping_address', 'payment', 'status', )
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'start_date', 'shipping_address', 'payment', 'status', )
 
-# class OrderProductAdmin(admin.ModelAdmin):
-#     list_display = ('order', 'item', 'quantity', )
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ('order', 'item', 'quantity', )
 
-# class AddressAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'street_address', 'city', )
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'street_address', 'city', )
     
-# class PaymentAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'amount', 'timestamp', )
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'timestamp', )
 
-# class ReviewAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'reviewtext', 'rating', )
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reviewtext', 'rating', )
 
-# admin.site.register(UserProfile, ProfileAdmin)
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Product, ProductAdmin)
-# admin.site.register(Cart, CartAdmin)
-# admin.site.register(Order, OrderAdmin)
-# admin.site.register(OrderProduct, OrderProductAdmin)
-# admin.site.register(Address, AddressAdmin)
-# admin.site.register(Payment, PaymentAdmin)
-# admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserProfile, ProfileAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct, OrderProductAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Review, ReviewAdmin)
--- Grocery Point of Sale System/core/views.py
+++ Grocery Point of Sale System/core/views.py
@@ -13,11 +13,434 @@ import datetime
 import random
 import string
 
+# Create your views here.
+def category_list():
+    categorylist = []
+    categorylist.append({
+        'menz': Category.objects.filter(gender='M')
+    })
+    categorylist.append({
+        'womens': Category.objects.filter(gender='F')
+    })
+    categorylist.append({
+        'kids': Category.objects.filter(gender='K')
+    })
+    categorylist.append({
+        'unisex': Category.objects.filter(gender='U')
+    })
+    
+    return categorylist
+
 
 def home(request):
-
+    categorylist = []
+    for category in Category.objects.all():
+        if Product.objects.filter(category=category).count() == 0:
+            continue
+        categorylist.append({
+            'category': category.name,
+            'products': Product.objects.filter(category=category)[:3]
+        })
+    
+    cartitems = 0
+    if request.user.is_authenticated:
+        cartitems = Cart.objects.filter(user=request.user).count()
 
     return render(request, 'home\index.html',{
-        'product': "gf",
-
+        'product': categorylist,
+        'cartitems': cartitems,
+        'categories': category_list()
     })
+
+def about(request):
+    cartitems = 0
+    if request.user.is_authenticated:
+        cartitems = Cart.objects.filter(user=request.user).count()
+
+    categorymenu = category_list()
+    return render(request, 'home/about.html',{
+        'cartitems': cartitems,
+        'categories': category_list()
+    })
+
+
+def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, 'Username or Password Incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
+
+
+def registerpage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            letters = string.digits
+            userinfo = UserProfile(user=authenticate(request, username=user, password = form.cleaned_data.get('password1')))
+            userinfo.stripe_customer_id = user + ''.join(random.choice(letters) for i in range(6))
+            userinfo.save()
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+@login_required(login_url='login')
+def addcart(request, slug):
+    product = Product.objects.get(slug=slug)
+    if product.quantity == 0:
+        return redirect(request.META['HTTP_REFERER'])
+    cartitem, notcreated = Cart.objects.get_or_create(item=product, user=request.user)
+    if notcreated == False: 
+        cartitem.quantity = cartitem.quantity + 1
+    cartitem.save()
+    product.quantity -= 1
+    product.sold += 1
+    product.save()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def productsearch(request, id):
+    category = Category.objects.get(id=id)
+    products = Product.objects.filter(category=category)
+
+    cartitems = 0
+    if request.user.is_authenticated:
+        cartitems = Cart.objects.filter(user=request.user).count()
+
+    return render(request, 'core\product.html',{
+        'category': category,
+        'cartitems': cartitems,
+        'product': products,
+        'categories': category_list()
+    })
+
+
+def product(request, slug):
+    product = Product.objects.get(slug=slug)
+    if request.method == 'POST' and request.POST.get("quantity"):
+        if request.user.is_authenticated == False:
+            return redirect("login")
+        qty = min(product.quantity, int(request.POST.get("quantity")))
+        cartitem, notcreated = Cart.objects.get_or_create(item=product, user=request.user)
+        if notcreated == False: 
+            cartitem.quantity = cartitem.quantity + qty
+        else:
+            cartitem.quantity = qty
+        cartitem.save()
+        product.quantity -= qty
+        product.sold += qty
+        product.save()
+    
+    reviews = Review.objects.filter(item=product)[:3]
+    if request.user.is_authenticated == True:
+        isreviewed = Review.objects.filter(item=product).filter(user=request.user).count()
+    else:
+        isreviewed = 1
+    reveiwnumber =  max(Review.objects.filter(item=product).count(), 0)
+
+    cartitems = 0
+    if request.user.is_authenticated:
+        cartitems = Cart.objects.filter(user=request.user).count()
+    products = Product.objects.filter(category=product.category)[:4]
+    
+    return render(request, 'core\index.html',{
+        'product': product,
+        'cartitems': cartitems,
+        'products': products,
+        'categories': category_list(),
+        'reviews': reviews,
+        'reviewnumber': reveiwnumber,
+        'isreviewed': isreviewed
+    })
+
+
+@login_required(login_url='login')
+def createreview(request, slug):
+    product = Product.objects.get(slug=slug)
+
+    review = Review()
+    review.user = request.user
+    review.item = product
+    review.reviewtext = request.POST.get("review")
+    review.rating = request.POST.get("ratings")
+    review.save()
+
+    product.rating = (product.rating + float(request.POST.get("ratings"))) / 2
+    product.save()
+
+    # cartitems = 0
+    # if request.user.is_authenticated:
+    #     cartitems = Cart.objects.filter(user=request.user).count()
+    # products = Product.objects.filter(category=product.category)
+
+    # reviews = Review.objects.filter(item=product)
+    # isreviewed = Review.objects.filter(item=product).filter(user=request.user).count()
+
+    return redirect("product", slug=slug)
+
+
+@login_required(login_url='login')
+def productaddcart(request, slug):
+    product = Product.objects.get(slug=slug)
+    if product.quantity == 0:
+        return redirect('home')
+    cartitem, notcreated = Cart.objects.get_or_create(item=product, user=request.user)
+    if notcreated == False:
+        cartitem.quantity = cartitem.quantity + 1
+    cartitem.save()
+    product.quantity -= 1
+    product.sold += 1
+    product.save()
+    cartitems = Cart.objects.filter(user=request.user).count()
+    products = Product.objects.filter(category=product.category)
+    return render(request, 'core\index.html',{
+        'product': product,
+        'cartitems': cartitems,
+        'products': products,
+        'categories': category_list()
+    })
+
+
+@login_required(login_url='login')
+def cart(request):
+    products = Cart.objects.filter(user=request.user)
+    if request.method == 'POST':
+        for item in products:
+            product = Product.objects.get(slug=item.item.slug)
+            qty = 0
+            if request.POST.get(item.item.slug):
+                if request.POST.get(item.item.slug) == '0':
+                    qty = -item.quantity
+                    item.delete()
+                else:
+                    qty = min(int(request.POST.get(item.item.slug))-item.quantity, product.quantity)
+                    item.quantity += qty
+                    item.save()
+            else:
+                qty = -item.quantity
+                item.delete()
+            product.quantity -= qty
+            product.sold += qty
+            product.save()
+
+    products = Cart.objects.filter(user=request.user)
+    cartitems = Cart.objects.filter(user=request.user).count()
+    total = 0.0
+    for product in products:
+        total += product.get_final_price()
+    return render(request, 'core\cart.html',{
+        'products': products,
+        'cartitems': cartitems,
+        'totalprice': total,
+        'total': total+50,
+        'categories': category_list()
+    })
+
+
+@login_required(login_url='login')
+def shipping(request):
+    products = Cart.objects.filter(user=request.user)
+    if request.method == 'POST':
+        total = 0.0
+        for item in products:
+            total += item.get_final_price()
+
+        letters = string.digits
+        created = True
+        customer = UserProfile.objects.get(user=request.user)
+        while created:
+            result_str = customer.stripe_customer_id + ''.join(random.choice(letters) for i in range(6))
+            created = Payment.objects.filter(stripe_charge_id=result_str).exists()
+
+        payment = Payment(user=request.user, amount=total+50, timestamp=datetime.datetime.now(), stripe_charge_id=result_str)
+        payment.save()
+
+        created = True
+        while created:
+            result_str = ''.join(random.choice(letters) for i in range(9))
+            created = Order.objects.filter(ref_code=result_str).exists()
+
+        order = Order(user=request.user, start_date=datetime.datetime.now(), status='RECEIVED')
+        order.ref_code = result_str
+        order.name = request.POST.get("name")
+        order.phone = "+88" + request.POST.get("phone")
+        order.shipping_address = Address.objects.get(id=request.POST.get("seladdress"))
+        order.payment = payment
+        order.save()
+
+        products = Cart.objects.filter(user=request.user)
+        for item in products:
+            orderproduct = OrderProduct(order=order, item=item.item, quantity=item.quantity)
+            orderproduct.save()
+            item.delete()
+
+        return redirect("/cart/")
+
+    products = Cart.objects.filter(user=request.user)
+    cartitems = Cart.objects.filter(user=request.user).count()
+    addresses = Address.objects.filter(user=request.user)
+
+    if cartitems == 0:
+        return redirect("cart")
+
+    total = 0.0
+    for product in products:
+        total += product.get_final_price()
+        
+    return render(request, 'core\shipping.html',{
+        'products': products,
+        'cartitems': cartitems,
+        'totalprice': total,
+        'total': total+50,
+        'addresses': addresses,
+        'categories': category_list()
+    })
+
+
+@login_required(login_url='login')
+def createaddress(request):
+    if request.method == 'POST':
+        address = Address()
+        address.user = request.user
+        address.country = request.POST.get("country")
+        address.city = request.POST.get("city")
+        address.area = request.POST.get("area")
+        address.street_address = request.POST.get("streetaddress")
+        address.save()
+    else:
+        return redirect("/shipping/")
+    return redirect("/shipping/")
+
+
+@login_required(login_url='login')
+def profile(request):
+    if request.method == 'POST':
+        userinfo = UserProfile.objects.get(user=request.user)
+        userinfo.fullname = request.POST.get("fullname")
+        userinfo.birthdate = request.POST.get("birthdate")
+        userinfo.gender = request.POST.get("gender")
+        userinfo.phone = request.POST.get("phone")
+        userinfo.save()
+
+    userinfo = UserProfile.objects.get(user=request.user)
+    if userinfo.birthdate != None:
+        birthdate = userinfo.birthdate.strftime('%Y-%m-%d')
+    else:
+        birthdate = datetime.datetime.now().strftime('%Y-%m-%d')
+    cartitems = Cart.objects.filter(user=request.user).count()
+    addresses = Address.objects.filter(user=request.user)
+    
+    return render(request, 'core\profile.html',{
+        'userinfo': userinfo,
+        'date':birthdate,
+        'user': request.user,
+        'cartitems': cartitems,
+        'addresses': addresses,
+        'categories': category_list()
+    })
+
+
+@login_required(login_url='login')
+def createaddressprofile(request):
+    if request.method == 'POST':
+        address = Address()
+        address.user = request.user
+        address.country = request.POST.get("country")
+        address.city = request.POST.get("city")
+        address.area = request.POST.get("area")
+        address.street_address = request.POST.get("streetaddress")
+        address.save()
+    else:
+        return redirect("/profile/")
+    return redirect("/profile/")
+
+
+@login_required(login_url='login')
+def deleteaddressprofile(request, id):
+    if request.method == 'POST':
+        Address.objects.filter(id=id).delete()
+    else:
+        return redirect("/profile/")
+    return redirect("/profile/")
+
+
+@login_required(login_url='login')
+def order(request):
+    orders = Order.objects.filter(user=request.user).order_by('-start_date')
+    ordercount = Order.objects.filter(user=request.user).count()
+    cartitems = Cart.objects.filter(user=request.user).count()
+    
+    orderlist = []
+    for order in orders:
+        orderlist.append({
+            'order': order,
+            'products': OrderProduct.objects.filter(order=order)
+        })
+
+    return render(request, 'core\order.html',{
+        'ordercount': ordercount,
+        'orders': orderlist,
+        'cartitems': cartitems,
+        'categories': category_list()
+    })
+
+
+@login_required(login_url='login')
+def trackorder(request, ref_code):
+    order = Order.objects.get(ref_code=ref_code)
+    orderproducts = OrderProduct.objects.filter(order=order)
+    orderproductcount = OrderProduct.objects.filter(order=order).count()
+    cartitems = Cart.objects.filter(user=request.user).count()
+    payment = Payment.objects.get(order=order).amount
+
+    return render(request, 'core\ordertrack.html',{
+        'ordercount': orderproductcount,
+        'orderproducts': orderproducts,
+        'order': order,
+        'totalprice': payment-50,
+        'total': payment,
+        'cartitems': cartitems,
+        'categories': category_list()
+    })
+
+
+@login_required(login_url='login')
+def cancelorder(request):
+    if request.method == 'POST':
+        order = Order.objects.get(ref_code=request.POST.get("ref_code"))
+        order.status = 'CANCELLED'
+        order.save()
+        orderproducts = OrderProduct.objects.filter(order=order)
+        for item in orderproducts:
+            product = Product.objects.get(slug=item.item.slug)
+            product.quantity += item.quantity
+            product.sold -= item.quantity
+            product.save()
+    else:
+        return redirect("/order/")
+    return redirect("/order/")
