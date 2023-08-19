ec0681043c510d22b1b5c0713704da64dee200ff
Enamul <syl.enamul@gmail.com>
2023-08-19 21:24:32 +0600

    Deploy to PythonAny

--- Grocery Point of Sale System/POS_System/settings.py
+++ Grocery Point of Sale System/POS_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v-c-eozrh&e*$a$=q(+)mav4(z-&!r+eckwy$8q+8d5xwff*1@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
