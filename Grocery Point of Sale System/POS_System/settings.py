294279d36b813824087962e754b2f2d02aa5a471
Enamul <syl.enamul@gmail.com>
2023-08-19 22:38:09 +0600

    commit2

--- Grocery Point of Sale System/POS_System/settings.py
+++ Grocery Point of Sale System/POS_System/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v-c-eozrh&e*$a$=q(+)mav4(z-&!r+eckwy$8q+8d5xwff*1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
