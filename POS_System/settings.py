0d65c3eb456df6a9e1a28e1c0c930fa2de39b366
Enamul <syl.enamul@gmail.com>
2023-08-20 19:56:26 +0600

    c02

--- POS_System/settings.py
+++ POS_System/settings.py
@@ -26,13 +26,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+## For example, for a site URL at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):https:///
+ALLOWED_HOSTS = ['web-production-45b2.up.railway.app', '127.0.0.1']
 
+# During development, you can instead set just the base URL
+# (you might decide to change the site a few times).
+# ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
 
 # Application definition
 
