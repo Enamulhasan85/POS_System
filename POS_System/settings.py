50501a6f9398e26b1b37225de0cb0bbc28e20f88
Enamul <syl.enamul@gmail.com>
2023-08-21 11:56:11 +0600

    ff

--- POS_System/settings.py
+++ POS_System/settings.py
@@ -159,7 +159,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
